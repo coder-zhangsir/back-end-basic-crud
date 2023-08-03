a95ce5c770861a8e0203fee8ead5c96554e8c3e0
ZhangSir <57878778@qq.com>
2023-08-03 22:03:59 +0800

    Project initialization(x3)

---
+++ index.js
@@ -0,0 +1,13 @@
+// 导入 express 模块
+const express = require('express')
+// 创建 express 的服务器实例
+const app = express()
+// 定义监听端口号
+const port = 3007
+
+// write your code here...
+
+// 调用 app.listen 方法，指定端口号并启动Web服务器
+app.listen(port, () => {
+    console.log(`Example app listening on port http://127.0.0.1:${port}`)
+})
